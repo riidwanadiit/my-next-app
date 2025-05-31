0a5cd9f5183333e58c46c70fc486fb7276ef2ff9
ridwansaputra <riidwanadiit@gmail.com>
2025-05-31 17:48:39 +0700

    gh-pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repo = "my-next";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/" + repo,
+  assetPrefix: "/" + repo + "/",
 };
 
 export default nextConfig;
