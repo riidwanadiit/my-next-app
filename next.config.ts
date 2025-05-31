171a3d4a8e127e2a6be7350ee2a14f2eeff4acfa
ridwansaputra <riidwanadiit@gmail.com>
2025-05-31 17:53:44 +0700

    gh-pages config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const repo = "my-next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   basePath: "/" + repo,
   assetPrefix: "/" + repo + "/",
